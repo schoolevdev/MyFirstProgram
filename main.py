a0ed2f7264c34d277e7fece0a76a5a278a5e5888
schoolevdev <loddere14207@stxavier.org>
2022-08-23 10:55:11 -0400

    class stuff

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-# This is a sample Python script that squares any number you type in.
+# This is a sample Python script that puts any number to any exponent.
 # This is my first Python Program (it isn't)
 # Name: Evin Lodder
 # Version/Date: 8/23/2022
 def main():
-    num: int = int(input("Type a number to square: "))
-    print(num ** 2)
+    num: int = int(input("Type a number: "))
+    exponent: int = int(input("Type the exponent: "))
+    print(num ** exponent)
+
+    # class stuff
+    print("Evin Lodder\n6326 Coach House Way\nLiberty Twp, Ohio 45011")
 
 
 if __name__ == "__main__":
