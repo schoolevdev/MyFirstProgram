6b25daedddd1e076b1ec4ed38bc1c998789ed1a5
schoolevdev <loddere14207@stxavier.org>
2022-08-23 10:46:56 -0400

    stuff

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+# This is my first Python Program (it isn't)
+# Name: Evin Lodder
+# Version/Date: 8/23/2022
+def main():
+    num: int = int(input("Type a number to square: "))
+    print(num ** 2)
+
+
+if __name__ == "__main__":
+    main()
