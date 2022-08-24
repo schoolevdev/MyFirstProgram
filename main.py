3f9312b27dacedf711db459062bd11f25392a05c
schoolevdev <loddere14207@stxavier.org>
2022-08-24 14:04:36 -0400

    class stuff

--- main.py
+++ main.py
@@ -2,13 +2,40 @@
 # This is my first Python Program (it isn't)
 # Name: Evin Lodder
 # Version/Date: 8/23/2022
+import os
+import random
 def main():
-    num: int = int(input("Type a number: "))
-    exponent: int = int(input("Type the exponent: "))
-    print(num ** exponent)
-
-    # class stuff
-    print("Evin Lodder\n6326 Coach House Way\nLiberty Twp, Ohio 45011")
+    os.system("cls")
+    choice: str = input("Rock, paper, or scissors: ").lower()
+    computerchoice: str = random.random(["rock", "paper", "scissors"])
+    match choice:
+        case "rock":
+            if computerchoice == "scissors":
+                print("You won!")
+            elif computerchoice == "paper":
+                print("You lost :(")
+            else: print("Tie.")
+            pass
+        case "paper":
+            if computerchoice == "rock":
+                print("You won!")
+            elif computerchoice == "scissors":
+                print("You lost :(")
+            else:
+                print("Tie.")
+            pass
+        case "scissors":
+            if computerchoice == "paper":
+                print("You won!")
+            elif computerchoice == "rock":
+                print("You lost :(")
+            else:
+                print("Tie.")
+            pass
+        case _:
+            print("That isn't rock, paper, or scissors!")
+            pass
+    main()
 
 
 if __name__ == "__main__":
