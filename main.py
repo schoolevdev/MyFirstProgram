98d45c7254250fe4b2387f09fe5b484860055c0b
schoolevdev <loddere14207@stxavier.org>
2022-08-26 08:55:08 -0400

    class stuff

--- main.py
+++ main.py
@@ -4,37 +4,39 @@
 # Version/Date: 8/23/2022
 import os
 import random
+import math
+
+choices: list = ["rock", "paper", "scissors"]
+win: dict = {
+    "rock": "scissors",
+    "paper": "rock",
+    "scissors": "paper"
+}
+lose: dict = dict([(value, key) for key, value in win.items()])
+
+
 def main():
     os.system("cls")
+    num: int = random.random()
+    while round(num, 12) != 0.999999999999:
+        num = random.random()
+    print(num)
+
+def main_fun():
+    os.system("cls")
     choice: str = input("Rock, paper, or scissors: ").lower()
-    computerchoice: str = random.random(["rock", "paper", "scissors"])
-    match choice:
-        case "rock":
-            if computerchoice == "scissors":
-                print("You won!")
-            elif computerchoice == "paper":
-                print("You lost :(")
-            else: print("Tie.")
-            pass
-        case "paper":
-            if computerchoice == "rock":
-                print("You won!")
-            elif computerchoice == "scissors":
-                print("You lost :(")
-            else:
-                print("Tie.")
-            pass
-        case "scissors":
-            if computerchoice == "paper":
-                print("You won!")
-            elif computerchoice == "rock":
-                print("You lost :(")
-            else:
-                print("Tie.")
-            pass
-        case _:
-            print("That isn't rock, paper, or scissors!")
-            pass
+    if choice not in choices:
+        print("That isn't a choice!")
+        main()
+    computer_choice: str = choices[random.randrange(0, 3)]
+    print(f"Computer chose {computer_choice}")
+
+    if win[choice] == computer_choice:
+        print("You won!")
+    elif lose[choice] == computer_choice:
+        print("You lost :(")
+    else:
+        print("Tie.")
     main()
 
 
